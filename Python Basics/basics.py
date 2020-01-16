9b405db30b748d598d5651ff033a29eeedd242ab
eilish-sinclair <eilishsinclair@gmail.com>
2020-01-16 14:22:14 +1300

    Learnt about Python basics

--- Python Basics/basics.py
+++ Python Basics/basics.py
@@ -1,3 +1,34 @@
-print('How are you?')
-print('o----')
-print(' ||||')
+# Here we are multiplying the string by number ten.
+# This code is also called an expression.
+# print('*' * 10)
+
+# The number Ten is an integer.
+# price = 10
+
+# The number Four Point Nine as a floating point number or floats for short.
+# rating = 4.9
+
+# The Cat is a string.
+# name = 'Cat'
+
+# The True and False are Booleans. When defining variables should always use lower case letters.
+# is_published = True
+# is_published = False
+
+# print(price)
+
+# Exercise One:
+# full_name = 'John Smith'
+# age = 20
+# is_new = True
+
+# Exercise Two:
+# message = input('How are you? ')
+# print('I am great ' + message)
+
+# Exercise Three a short questionnaire:
+name = input('Who are you? ')
+favourite_animal = input('Whats your favourite animal? ')
+favourite_travel = input('Where is your favourite place to travel too? ')
+message = input('I would like to write ')
+print(name + ' likes ' + favourite_animal + ' and loves ' + favourite_travel + ' Thank you so much for watching! ')
