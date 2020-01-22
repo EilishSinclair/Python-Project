934f52c1fd00368b538b95f8e310f82a2b42961a
eilish-sinclair <eilishsinclair@gmail.com>
2020-01-22 15:21:07 +1300

    Learning about Arithmetic Operations

--- Python Basics/basicstwo.py
+++ Python Basics/basicstwo.py
@@ -1,1 +1,15 @@
-print('Hello')
+#Arithmetic Operations
+# Adding
+print(10 + 3)
+# Subtracting
+print(10 - 3)
+# Multiplication
+print(10 * 3)
+# Division One Way (Floating point number)
+print(10 / 3)
+# Division The Other way (An Integer)
+print(10 // 3)
+# Module list (Returns remainder of the division)
+print(10 % 3)
+# Exponent (Which is the power)
+print(10 ** 3)
