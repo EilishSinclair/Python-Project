cb8a2ca8f7fe07ba423dea178e29275dd7fdd269
eilish-sinclair <eilishsinclair@gmail.com>
2020-01-23 13:46:42 +1300

    Logical Operations

--- Python Basics/basicstwo.py
+++ Python Basics/basicstwo.py
@@ -1,4 +1,4 @@
-#Arithmetic Operations
+# Exercise One Arithmetic Operations
 # Adding
 print(10 + 3)
 # Subtracting
@@ -11,5 +11,42 @@ print(10 / 3)
 print(10 // 3)
 # Module list (Returns remainder of the division)
 print(10 % 3)
-# Exponent (Which is the power)
+# Exponent (Is the power)
 print(10 ** 3)
+# Augmented assignment operator.
+x = 10
+x += 3
+print(x)
+
+# Exercise Two Operator Precedence
+x = (10 + 3) * 2 ** 2
+print(x)
+# parenthesis
+# exponentiation 2 ** 3
+# multiplication or division
+# addition or subtraction
+
+# Exercise Three Math Functions
+import math
+print(math.floor(2.9))
+
+# Exercise Four If Statements
+is_happy = False
+is_excited = False
+
+if is_happy:
+    print("Glad your happy today")
+    print("Be happy everyday")
+elif is_excited:
+    print("Have excitement")
+    print("Make sure to become excited")
+else:
+    print("It's a beautiful day today!")
+
+print("Remember to be happy tomorrow ")
+
+# Exercise Five Logical Operators
+has_good_credit = True
+has_great_house = False
+if has_good_credit and not has_great_house:
+    print("Eligible for loan")
