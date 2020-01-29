ba1b16af75ac4d0630fb01d9f0ac0752a9b4a7b7
eilish-sinclair <eilishsinclair@gmail.com>
2020-01-29 15:48:36 +1300

    Updated and reorganised file one

--- Python Basics/basicsone.py
+++ Python Basics/basicsone.py
@@ -1,26 +1,26 @@
 # Exercise One Variables (Integers):
 # The number Ten is an integer.
-# price = 10
+price = 10
 
 # Exercise Two Variables (Floats):
 # The number Four Point Nine as a floating point number or floats for short.
-# rating = 4.9
+rating = 4.9
 
 # Exercise Three Variables (Strings):
 # The Cat is a string.
-# name = 'Cat'
+name = 'Cat'
 
 # Exercise Four Variables (Booleans):
 # The True and False are Booleans. When defining variables should always use lower case letters.
-# is_published = True
-# is_published = False
+is_published = True
+is_published = False
 
-# print(price)
+print(price)
 
 # Exercise Five Receiving Input:
-# full_name = 'John Smith'
-# age = 20
-# is_new = True
+full_name = 'John Smith'
+age = 20
+is_new = True
 
 # Exercise Six Python Cheat Sheet:
 name = input('Who are you? ')
@@ -30,30 +30,19 @@ message = input('I would like to write ')
 print(name + ' likes ' + favourite_animal + ' and loves ' + favourite_travel + ' Thank you so much for watching! ')
 
 #Exercise Seven Type Conversion:
-# weight_lbs = input('Weight (lbs): ')
-# weight_kg = int(weight_lbs) * 0.45
-# print(weight_kg)
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
 
 # Exercise Eight Strings:
-# You need to use double quites when using an apostrophe.
-# Negative one is the index for the last character.
-# course = 'How to become a star'
-# another = course[1:5]
+# You need to use double quites when using an apostrophe, negative one is the index for the last character.
+course = 'How to become a star'
+another = course[1:5]
 
 # Exercise Nine Formatted strings:
 # A formatted string is f'' and example: f'{first_name} [{last_name}] is funny'
-# dog_name = 'Snow White'
-# dog_interest = 'Sleeping'
-# message = dog_name + ' [' + dog_interest + '] loves it! '
-# msg = f'{dog_name} [{dog_interest}] loves it!'
-# print(msg)
-
-# Exercise Ten String Methods
-# course = 'How to become stars'
-#len()
-# course.upper()
-# course.lower()
-# course.title()
-# course.find()
-# course.replace()
-# '....' in course
+dog_name = 'Snow White'
+dog_interest = 'Sleeping'
+message = dog_name + ' [' + dog_interest + '] loves it! '
+msg = f'{dog_name} [{dog_interest}] loves it!'
+print(msg)
