1ef2552342ac109a2b35ff2e9f41da5d6da51066
eilish-sinclair <eilishsinclair@gmail.com>
2020-01-30 15:37:04 +1300

    Putting car game together

--- Python Games/gametwo.py
+++ Python Games/gametwo.py
@@ -1,1 +1,16 @@
 # Building The Car Game
+command = ""
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        print("Car Started...")
+    elif command == "stop":
+        print("Car stopped.")
+    elif command == "help":
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to quit
+        """)
+    else:
+        print("I don' understand that!")
