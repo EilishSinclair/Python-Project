5f84b8e4778e84bcf22a6b6901b31ddb2f07a56f
eilish-sinclair <eilishsinclair@gmail.com>
2020-02-12 14:20:17 +1300

    Learning about For Loops

--- Python Basics/basicsthree.py
+++ Python Basics/basicsthree.py
@@ -1,1 +1,9 @@
-# Exercise One For Loops
+# Exercise One For Loops (Shopping Cart)
+prices = [10, 20, 30]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
+# Exercise Two Nested Loops
