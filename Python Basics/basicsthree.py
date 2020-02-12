64124ccf263df9c63f597d8650a2b2698adf43b7
eilish-sinclair <eilishsinclair@gmail.com>
2020-02-12 16:05:00 +1300

    Learning about nested loops

--- Python Basics/basicsthree.py
+++ Python Basics/basicsthree.py
@@ -1,4 +1,4 @@
-# Exercise One For Loops (Shopping Cart)
+# Exercise One For Loops (Supermarket Cart)
 prices = [10, 20, 30]
 
 total = 0
@@ -7,3 +7,21 @@ for price in prices:
 print(f"Total: {total}")
 
 # Exercise Two Nested Loops
+#(x, y)
+#(0, 0)
+#(0, 1)
+#(0, 2)
+#(1, 0)
+#(1, 1)
+#(1, 2)
+# for x in range(4):
+    # for y in range(5):
+        # print(f'({x}, {y})')
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+
