b9c02d6b3a4060f06390d64050fcb73b0ed85a61
eilish-sinclair <eilishsinclair@gmail.com>
2020-02-17 11:40:09 +1300

    Exercise Four Two D Lists Completed

--- Python Basics/basicsthree.py
+++ Python Basics/basicsthree.py
@@ -33,3 +33,13 @@ for number in numbers:
     if number > max:
         max = number
 print(max)
+
+# Exercise Four Two D Lists
+matrix = [
+     [1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]
+]
+for row in matrix:
+    for item in row:
+        print(item)
