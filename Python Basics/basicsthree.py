7fea82ccbdc699db1ebb87ec7334f15ee0aced92
eilish-sinclair <eilishsinclair@gmail.com>
2020-02-18 11:39:36 +1300

    Completed List Methods

--- Python Basics/basicsthree.py
+++ Python Basics/basicsthree.py
@@ -45,7 +45,9 @@ for row in matrix:
         print(item)
 
 # Exercise Five List Methods
-numbers = [5, 2, 1, 5, 7, 4]
-numbers2 = numbers.copy()
-numbers.append(10)
-print(numbers2)
+numbers = [2, 2, 4, 6, 3, 4, 6, 1]
+uniques = []
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)
