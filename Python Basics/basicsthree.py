dabcb765cdc56bb7f681ac015f52be5d5a362d13
eilish-sinclair <eilishsinclair@gmail.com>
2020-05-08 14:38:56 +1200

    Added a happy face emoji and sad face emoji

--- Python Basics/basicsthree.py
+++ Python Basics/basicsthree.py
@@ -79,27 +79,13 @@ print(uniques)
 #print(output)
 
 # Exercise Seven Emoji Converter
-# message = input(">")
-# words = message.split(' ')
-# emojis = {
-    # ":/": "🥰",
-    # ":(": "🏆️",
-    # ":)": "👩‍🦽🧑🏻‍🦽🧑🏼‍🦽🚶🏽‍♀️🧑🏽‍🦯🏃🏼‍♀️🧑🏾‍🦼🕴🏽👩🏻‍🦽🧑🏿‍🦼"
-# }
-# output = ""
-# for word in words:
-    # output += emojis.get(word, word) + " "
-# print(output)
-
-# Exercise Eight Functions
-# def greet_user():
-    # print('How are you?')
-    # print('How is your day going?')
-
-
-# print("Start")
-# greet_user()
-# print("Finish")
-
-# Exercise Nine Parameters
-def greet_user(name):
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)": "🥰",
+    ":(": "✈️"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
