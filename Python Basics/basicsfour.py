f94e283a6e10704e1f1ec00839453c417a77391e
eilish-sinclair <eilishsinclair@gmail.com>
2020-05-11 15:45:38 +1200

    First exercise Keyword Arguments

---
+++ Python Basics/basicsfour.py
@@ -0,0 +1,8 @@
+# Exercise Four Keyword Arguments
+def greet_user(first_name, last_name):
+    print(f'Hello and welcome {first_name} {last_name}')
+    print(' To London, England!')
+
+print("Start")
+greet_user("Jan", last_name="Golding")
+print("Finish")
