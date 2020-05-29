e24029e4ac07055b7280f23a94af882527fd0d4b
eilish-sinclair <eilishsinclair@gmail.com>
2020-05-29 15:50:58 +1200

    Have added six exercies into this file

--- Python Basics/basicsthree.py
+++ Python Basics/basicsthree.py
@@ -1,4 +1,23 @@
- # Exercise One For Loops (Supermarket Cart)
+# Exercise One Math Functions:
+import math
+print(math.floor(2.9))
+
+# Exercise Two If Statements:
+is_happy = False
+is_excited = False
+
+if is_happy:
+    print("Glad your happy today")
+    print("Be happy everyday")
+elif is_excited:
+    print("Have excitement")
+    print("Make sure to become excited")
+else:
+    print("It's a beautiful day today!")
+
+print("Remember to be happy tomorrow ")
+
+# Exercise Three For Loops (Supermarket Cart)
 prices = [10, 20, 30]
 
 total = 0
@@ -6,7 +25,7 @@ for price in prices:
     total += price
 print(f"Total: {total}")
 
-# Exercise Two Nested Loops
+# Exercise Four Nested Loops
  # (x, y)
  # (0, 0)
  # (0, 1)
@@ -26,7 +45,7 @@ for x_count in numbers:
        output += 'x'
     print(output)
 
-# Exercise Three Lists
+# Exercise Five Lists
 numbers = [ 3, 6, 10, 2, 8, 4,]
 max = numbers[0]
 for number in numbers:
@@ -34,7 +53,7 @@ for number in numbers:
         max = number
 print(max)
 
-# Exercise Four Two D Lists
+# Exercise Six Two D Lists
 matrix = [
      [1, 2, 3],
      [4, 5, 6],
@@ -43,72 +62,3 @@ matrix = [
 for row in matrix:
     for item in row:
         print(item)
-
-# Exercise Five List Methods
-numbers = [2, 2, 4, 6, 3, 4, 6, 1]
-uniques = []
-for number in numbers:
-    if number not in uniques:
-        uniques.append(number)
-print(uniques)
-
-# Exercise Four Tuples
-# numbers = (1, 2, 3)
-# print(numbers[0])
-
-# Exercise Five Unpacking
-# coordinates = [1, 2, 3,]
-# x, y, z = coordinates
-# print(z)
-
-# Exercise Six Dictionaries
-# Key Value Pairs
-# Name: Daisy Flower
-# Email: daisy@gmail.com
-# Phone: 01234
-# phone = input("Phone: ")
-# digits_mapping = {
- #"1": "One",
- #"2": "Two",
- #"3": "Three",
- #"4": "Four"
-#}
-#output = ""
-#for ch in phone:
-    #output += digits_mapping.get(ch, "!") + " "
-#print(output)
-
-# Exercise Seven Emoji Converter
-message = input(">")
-words = message.split(' ')
-emojis = {
-    ":)": "🥰",
-    ":(": "✈️"
-}
-output = ""
-for word in words:
-    output += emojis.get(word, word) + " "
-print(output)
-
-# Exercise Eight Functions
-def greet_user():
-    print('Welcome to....')
-    print('New York City!')
-    print('Have a great trip.')
-
-
-print("Start here")
-greet_user()
-print('Finish here')
-
-# Exercise Nine Parameters
-def greet_user(first_name, last_name):
-    print(f'Welcome {first_name} {last_name} to')
-    print('New York City!')
-    print('Have a great trip.')
-
-
-print("Start here")
-greet_user("Daisy", "Disney")
-greet_user("Franklin", "Disney")
-print('Finish here')
