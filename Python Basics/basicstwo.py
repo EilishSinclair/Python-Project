f2086e2297914961bf692fa69b05c1e734e824fd
eilish-sinclair <eilishsinclair@gmail.com>
2020-05-29 15:47:46 +1200

    Have added six exercises

--- Python Basics/basicstwo.py
+++ Python Basics/basicstwo.py
@@ -1,4 +1,22 @@
-# Exercise One String Methods:
+#Exercise One Type Conversion:
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
+# Exercise Two Strings:
+# You need to use double quites when using an apostrophe, negative one is the index for the last character.
+course = 'How to become a star'
+another = course[1:5]
+
+# Exercise Three Formatted strings:
+# A formatted string is f'' and example: f'{first_name} [{last_name}] is funny'
+dog_name = 'Snow White'
+dog_interest = 'Sleeping'
+message = dog_name + ' [' + dog_interest + '] loves it! '
+msg = f'{dog_name} [{dog_interest}] loves it!'
+print(msg)
+
+# Exercise Four String Methods:
 course = 'How to become stars'
 # len()
 # course.upper()
@@ -8,7 +26,7 @@ course = 'How to become stars'
 # course.replace()
 # '....' in course
 
-# Exercise Two Arithmetic Operations:
+# Exercise Five Arithmetic Operations:
 # Adding
 print(10 + 3)
 # Subtracting
@@ -28,63 +46,10 @@ x = 10
 x += 3
 print(x)
 
-# Exercise Three Operator Precedence:
+# Exercise Six Operator Precedence:
 x = (10 + 3) * 2 ** 2
 print(x)
 # parenthesis
 # exponentiation 2 ** 3
 # multiplication or division
 # addition or subtraction
-
-# Exercise Four Math Functions:
-import math
-print(math.floor(2.9))
-
-# Exercise Five If Statements:
-is_happy = False
-is_excited = False
-
-if is_happy:
-    print("Glad your happy today")
-    print("Be happy everyday")
-elif is_excited:
-    print("Have excitement")
-    print("Make sure to become excited")
-else:
-    print("It's a beautiful day today!")
-
-print("Remember to be happy tomorrow ")
-
-# Exercise Six Logical Operators
-has_good_background = True
-has_good_references = True
-
-if has_good_background and not has_good_references:
-    print ("Eligible for adoption")
-
-# Exercise Seven Comparison Operators
-name = "Bruce"
-
-if len(name) < 3:
-    print("Name has to be at least 3 characters")
-elif len(name) > 50:
-    print("Name must be a maximum of 50 characters.")
-else:
-    print("Name looks good!")
-
-# Exercise Eight Weight Converter
-weight = int(input('Weight: '))
-unit = input('(L)bs or (K)g: ')
-if unit.upper() == "L":
-    converted = weight * 0.45
-    print(f"Your weight is {converted} kilos")
-else:
-    converted = weight / 0.45
-    print(f"Your weight is {converted} pounds")
-
-# Exercise Nine While Loops
-guess_count = 1
-while guess_count <= 5:
-    print ('*' * guess_count)
-    guess_count = guess_count + 1
-print("Completed")
